190cd3e762189f7840e3c9d97d0c638eb93bcabd
Jakub Woźny <jakubovip@gmail.com>
2019-11-10 18:38:24 +0100

    Add basic operators' examples

---
+++ 3-basic-examples.ts
@@ -0,0 +1,56 @@
+import { interval, Observable, pipe, range } from 'rxjs';
+import { filter, map, reduce, scan, take } from 'rxjs/operators';
+
+function successfulObservable() {
+    const obs = new Observable(subscriber => {
+        subscriber.next('first value');
+        subscriber.next('another value');
+        subscriber.next('yet another value');
+
+        subscriber.complete();
+    });
+
+    obs.subscribe(value => console.log(value),
+        error => undefined,
+        () => console.log('completed'));
+}
+
+
+function failedObservable() {
+    const obs = new Observable(subscriber => {
+        subscriber.next('first value');
+
+        subscriber.error('oops!');
+
+        subscriber.next('another one?');
+
+        subscriber.complete();
+    });
+
+    obs.subscribe(value => console.log(value),
+        error => console.log(error),
+        () => console.log('completed'));
+}
+
+
+// ARRAY LIKE OPERATORS
+
+function basicOpsExample() {
+    range(0, 10) // or interval/timer
+        .pipe(
+            // map(value => value * value),
+            // filter(value => value % 2 === 1),
+            // count(value => value > 10),
+            // reduce(((acc, value) => acc + value)),
+            // scan(((acc, value) => acc + value)),
+            // max()
+        )
+        .subscribe(console.log);
+
+}
+
+
+// successfulObservable();
+// failedObservable();
+
+// basicOpsExample();
